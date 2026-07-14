f27cd747e05654f1eef25f57905122aa70494cd4
Wilo92 <wilmerrestrepoo@hotmail.com>
2026-07-14 13:50:05 -0500

    update 14 jul

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+       
+        source: "/:all*(mp4|webp|png|jpg|jpeg|ico|svg)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2592000, stale-while-revalidate=86400",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
